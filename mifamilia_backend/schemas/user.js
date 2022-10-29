c2fbb7f7f3f8bb20b8f7e115a1f5a2cfed2db7c3
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-10-29 23:17:31 +0200

    Google signin implemented, still in progress.

--- mifamilia_backend/schemas/user.js
+++ mifamilia_backend/schemas/user.js
@@ -1,17 +1,17 @@
-export default{
-    name: "user",
-    title: "User",
-    type: "document",
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
     fields: [
-        {
-            name: "userName",
-            title: "UserName",
-            type: "string",
-        },
-        {
-            name: "image",
-            title: "Image",
-            type: "string",
-        },
+      {
+        name: 'userName',
+        title: 'UserName',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'string',
+      },
     ]
-}
+  }
--- mifamilia_frontend/src/components/Login.jsx
+++ mifamilia_frontend/src/components/Login.jsx
@@ -2,21 +2,37 @@ import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
-import shareVideo from '../assets/share.mp4';
+import mifamiliaVideo from '../assets/mifamilia.mp4';
 import logo from '../assets/logofamilia.png';
+
+import { client } from "../client";
 
 
 const Login = () => {
   const navigate = useNavigate();
   const responseGoogle = (response) => {
-    console.log(response);
+    localStorage.setItem('user', JSON.stringify(response.profileObj));
+
+    const { name, googleId, imageUrl } = response.profileObji;
+
+    const doc = {
+      _id: googleId,
+      _type: 'user',
+      userName: name,
+      image: imageUrl,
+    };
+
+    client.createIfNotExists(doc)
+    .then(() => {
+      navigate('/', { replace: true });
+    });
   };
 
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className=" relative w-full h-full">
         <video
-          src={shareVideo}
+          src={mifamiliaVideo}
           type="video/mp4"
           loop
           controls={false}
@@ -25,9 +41,9 @@ const Login = () => {
           className="w-full h-full object-cover"
         />
 
-        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
           <div className="p-5">
-            <img src={logo} width="190px" className="rounded-lg" />
+            <img src={logo} width="290px" className="rounded-lg" />
           </div>
 
           <div className="shadow-2xl">
