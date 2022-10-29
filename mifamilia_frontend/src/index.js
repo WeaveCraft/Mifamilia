fb63c8cb61547649ec13eb1adb575d5919d75991
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-10-29 23:17:52 +0200

    Google login on its way.

--- mifamilia_frontend/src/index.js
+++ mifamilia_frontend/src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
-<Router>
-<App />
-</Router>,
+createRoot(document.getElementById('root')).render(
+    <Router>
+        <App />
+    </Router>
+)
 
- document.getElementById("root")
- );
+// ReactDOM.render(
+// <Router>
+// <App />
+// </Router>,
+
+//  document.getElementById("root")
+//  );
