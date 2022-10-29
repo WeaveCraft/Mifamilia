b0bca98f236752d1bbad0a3df52661d20c86915e
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-10-29 15:51:01 +0200

    Assets, Login & Home added.

---
+++ mifamilia_frontend/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { fcGoogle } from "react-icons/fc";
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
