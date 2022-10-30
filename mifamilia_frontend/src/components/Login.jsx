93174aa546d4e67d29198d5555464847ef409142
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-10-30 21:47:29 +0100

    Google Login Works!

--- mifamilia_frontend/src/components/Login.jsx
+++ mifamilia_frontend/src/components/Login.jsx
@@ -1,73 +1,64 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
+import {GoogleLogin, GoogleOAuthProvider} from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
-import { FcGoogle } from 'react-icons/fc';
-import mifamiliaVideo from '../assets/mifamilia.mp4';
+import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logofamilia.png';
-
-import { client } from "../client";
-
+import { client } from '../client';
+import jwt_decode from 'jwt-decode'
 
 const Login = () => {
-  const navigate = useNavigate();
-  const responseGoogle = (response) => {
-    localStorage.setItem('user', JSON.stringify(response.profileObj));
+    const navigate = useNavigate();
 
-    const { name, googleId, imageUrl } = response.profileObji;
+    const responseGoogle = (response) => {
 
-    const doc = {
-      _id: googleId,
-      _type: 'user',
-      userName: name,
-      image: imageUrl,
+        const userResponse = jwt_decode(response.credential);
+
+        localStorage.setItem('user', JSON.stringify(userResponse));
+        const { name, sub, picture } = userResponse;
+
+        const doc = {
+            _id: sub,
+            _type: 'user',
+            userName: name,
+            image: picture
+        }
+        
+        client.createIfNotExists(doc).then(()=>{
+            navigate('/', {replace: true})
+        });
+
     };
 
-    client.createIfNotExists(doc)
-    .then(() => {
-      navigate('/', { replace: true });
-    });
-  };
+    return (
+        <div className="flex justify-start items-center flex-col h-screen">
+            <div className=" relative w-full h-full">
+                <video
+                    src={shareVideo}
+                    type="video/mp4"
+                    loop
+                    controls={false}
+                    muted
+                    autoPlay
+                    className="w-full h-full object-cover"
+                />
 
-  return (
-    <div className="flex justify-start items-center flex-col h-screen">
-      <div className=" relative w-full h-full">
-        <video
-          src={mifamiliaVideo}
-          type="video/mp4"
-          loop
-          controls={false}
-          muted
-          autoPlay
-          className="w-full h-full object-cover"
-        />
+                <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+                    <div className="p-5">
+                        <img src={logo} width="130px" alt='logo' />
+                    </div>
 
-        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
-          <div className="p-5">
-            <img src={logo} width="290px" className="rounded-lg" />
-          </div>
-
-          <div className="shadow-2xl">
-            <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
-              render={(renderProps) => (
-                <button
-                  type="button"
-                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                >
-                  <FcGoogle className="mr-4" /> Sign in with google
-                </button>
-              )}
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy="single_host_origin"
-            />
-          </div>
+                    <GoogleOAuthProvider
+                        clientId={`${process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}`} className="shadow-2xl">
+                        <GoogleLogin
+                            onSuccess={responseGoogle}
+                            onFailure={responseGoogle}
+                            cookiePolicy="single_host_origin"
+                        />
+                    </GoogleOAuthProvider>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
--- mifamilia_frontend/src/index.js
+++ mifamilia_frontend/src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import App from "./App";
 import "./index.css";
 
+
 createRoot(document.getElementById('root')).render(
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
+
     <Router>
         <App />
     </Router>
+    </GoogleOAuthProvider>
 )
 
 // ReactDOM.render(
