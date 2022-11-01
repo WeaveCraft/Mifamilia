02e4ca94124fb6470d38eae24e4dd46cbd4e0b27
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-11-01 13:09:34 +0100

    View pin details.

--- mifamilia_frontend/src/components/CreatePin.jsx
+++ mifamilia_frontend/src/components/CreatePin.jsx
@@ -186,7 +186,7 @@ const CreatePin = ({ user }) => {
               <button
                 type="button"
                 onClick={savePin}
-                className="bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none"
+                className="bg-green-500 text-white font-bold p-2 rounded-full w-28 outline-none"
               >
                 Save Pin
               </button>
--- mifamilia_frontend/src/utils/data.js
+++ mifamilia_frontend/src/utils/data.js
@@ -1,11 +1,91 @@
-export const userQuery = (userId) => {
-    const query = `*[_type == "user" && _id == '${userId}']`;
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+  image{
+    asset->{
+      url
+    }
+  },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    } `;
 
-    return query;
-}
+export const pinDetailQuery = (pinId) => {
+  const query = `*[_type == "pin" && _id == '${pinId}']{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    title, 
+    about,
+    category,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+   save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    }
+  }`;
+  return query;
+};
+
+export const pinDetailMorePinQuery = (pin) => {
+  const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
 
 export const searchQuery = (searchTerm) => {
-    const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*'] {
+  const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
         image{
           asset->{
             url
@@ -26,30 +106,61 @@ export const searchQuery = (searchTerm) => {
                 image
               },
             },
-        }`
+          }`;
+  return query;
+};
 
-    return query;
-}
+export const userQuery = (userId) => {
+  const query = `*[_type == "user" && _id == '${userId}']`;
+  return query;
+};
 
-export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
     image{
-        asset->{
-          url
-    }
-        },
-          _id,
-          destination,
-          postedBy->{
-            _id,
-            userName,
-            image
-        },
-          save[]{
-            _key,
-            postedBy->{
-              _id,
-              userName,
-              image
-        },
+      asset->{
+        url
+      }
     },
-}`
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+export const userSavedPinsQuery = (userId) => {
+  const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
