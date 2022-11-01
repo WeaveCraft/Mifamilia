a6b5a97d15eb64317e930209618038f3e2af5f84
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-11-01 19:29:25 +0100

    Changed icons on homepage.

--- mifamilia_frontend/src/components/SideBar.jsx
+++ mifamilia_frontend/src/components/SideBar.jsx
@@ -41,7 +41,7 @@ const Sidebar = ({ closeToggle, user }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
-              <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" />
+              {category.image}
               {category.name}
             </NavLink>
           ))}
